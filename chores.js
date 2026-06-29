11c6f572f39736d598f007c9b1c6bf73d5489960
Jordan Martin <jmartin10297@yahoo.com>
2026-06-29 14:17:25 -0400

    Initial Working Chore Tracker

---
+++ chores.js
@@ -0,0 +1,35 @@
+const defaultChores = [
+    {
+        id: 1,
+        name: "Take out the Trash",
+        assignedTo: 1,
+        rotationGroup: "parents",
+        dueDate: "2026-07-04",
+        intervalDays: 7,
+        active: true,
+        completed: false,
+        lastCompleted: null
+    },
+    {
+        id: 2,
+        name: "Wash the Dishes",
+        assignedTo: 2,
+        rotationGroup: "peytonOnly",
+        dueDate: "2026-06-30",
+        intervalDays: 7,
+        active: true,
+        completed: false,
+        lastCompleted: null
+    },
+    {
+        id: 3,
+        name: "Vacuum the Living Room",
+        assignedTo: 3,
+        rotationGroup: "kids",
+        dueDate: "2026-06-28",
+        intervalDays: 7,
+        active: true,
+        completed: false,
+        lastCompleted: null
+    },
+];
